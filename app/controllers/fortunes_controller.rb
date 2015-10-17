d0f966c7f58e1a139272a526e2f7fea099ad7b1d
jordi92500 <jordi92500@hotmail.fr>
2015-10-17 17:57:38 -0500

    Add of Params

--- app/controllers/fortunes_controller.rb
+++ app/controllers/fortunes_controller.rb
@@ -1,5 +1,8 @@
 class FortunesController < ApplicationController
   def lucky
+    @first_name = params["first_name"]
+    @last_name = params["last_name"]
+    @color = params["color"]
     @numbers = [rand(100),rand(100),rand(100),rand(100)]
   end
 end
